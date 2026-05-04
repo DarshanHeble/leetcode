de78aaab9a5f2e4127d65fa48efa3d05592b3ff0
Darshan Heble <109078053+DarshanHeble@users.noreply.github.com>
2026-05-05 01:32:13 +0530

    Time: 1 ms (58.91%), Space: 48.8 MB (15.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,30 @@
 class Solution {
-    public int trap(int[] h) {
-        int sum = 0, rightMax = 0, leftMax = 0;
-        int n = h.length;
+    public int trap(int[] height) {
+        int n = height.length;
         int[] left = new int[n];
         int[] right = new int[n];
+        int count = 0;
 
-        // Left Array
+        int leftMax = 0;
+        int rightMax = 0;
+
         for (int i = 0; i < n; i++) {
             left[i] = leftMax;
-            leftMax = Math.max(leftMax, h[i]);
+            leftMax = Math.max(height[i], leftMax);
         }
 
-        // right Array
-        for (int i = n - 1; i > -1; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             right[i] = rightMax;
-            rightMax = Math.max(rightMax, h[i]);
+            rightMax = Math.max(height[i], rightMax);
         }
 
         for (int i = 0; i < n; i++) {
             int val = Math.min(left[i], right[i]);
 
-            sum += Math.max(0, val - h[i]);
+            // Remove block from val, make sure its not smaller then 0(otherwise negative)
+            count += Math.max(0, val - height[i]);
         }
 
-
-        return sum;
+        return count;
     }
 }
